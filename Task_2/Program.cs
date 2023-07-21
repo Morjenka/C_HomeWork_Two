a966f3fd175ddec5dd7dfc1d9af78acedf50f147
Marina <morjenka-z@mail.ru>
2023-07-21 19:40:29 +0300

    Программа работает. Третья цифра числа

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,16 +1,23 @@
-﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
-Console.Write(("Введите трёхзначное число: "));
+
+Console.WriteLine(("Введите число: "));
 int number = int.Parse(Console.ReadLine());
 if (number >= 100 && number <1000)
 {
     int res = (number % 10);
     Console.Write( "-> "+res);
-
 }
-else
+if (number >= 1000 && number <10000)
 {
-    Console.Write("третьей цифры нет");
+    int res = (number % 100 - number % 10)/10;
+    Console.Write( "-> "+res);
+}
+if (number >= 10000 && number <100000)
+{
+    int res = (number % 1000 - number % 10)/100;
+    Console.Write( "-> "+res);
 }
